711c664a39722124185fb0a225eeec1178a04571
diegooriani <diegooriani@Diegos-MacBook-Pro.local>
2017-07-04 12:59:53 +0100

    First commit.

---
+++ dist/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+  // fullpage customization
+  $('#fullpage').fullpage({
+    sectionsColor: ['', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    css3: true,
+    controlArrows: false,
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    menu: '#menu',
+
+  });
+
+  //Fullscreen
+  var requestFullscreen = function(ele) {
+    if (ele.requestFullscreen) {
+      ele.requestFullscreen();
+    } else if (ele.webkitRequestFullscreen) {
+      ele.webkitRequestFullscreen();
+    } else if (ele.mozRequestFullScreen) {
+      ele.mozRequestFullScreen();
+    } else if (ele.msRequestFullscreen) {
+      ele.msRequestFullscreen();
+    } else {
+      console.log('Fullscreen API is not supported.');
+    }
+  };
+
+  var exitFullscreen = function() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else {
+      console.log('Fullscreen API is not supported.');
+    }
+  };
+
+  var fsDocButton = document.getElementById('fs-doc-button');
+  var fsExitDocButton = document.getElementById('fs-exit-doc-button');
+
+  fsDocButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    requestFullscreen(document.documentElement);
+  });
+
+  fsExitDocButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    exitFullscreen();
+  });
+
+  var fsVidButton = document.getElementById('fs-vid-button');
+  var video = document.getElementById('video');
+
+  fsVidButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    requestFullscreen(video);
+  });
+});
+
+
